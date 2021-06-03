53d2497c738895f1498a7322f98f6cdee462bf44
Vundela Subbareddy <82031292+VundelaSubbareddy@users.noreply.github.com>
2021-06-03 13:56:22 +0530

    Add files via upload

---
+++ Assets/Scripts/BirdsManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdsManager : MonoBehaviour
+{
+    public GameObject prefab;
+    public List<BirdsMove> allPrefabs = new List<BirdsMove>();
+   // public float Seperationdis = 2f;
+    public Vector3 SpawnPos = new Vector3(2,0,2);
+    public Vector3 GoalPos;
+    public int NumberofBirds = 10;
+    public float MovementSpeed = 1f;
+    private void Start()
+    {
+        for (int j = 0; j < NumberofBirds; j ++)
+        {
+            var random = Random.insideUnitSphere;
+            Vector3 pos = new Vector3(random.x * SpawnPos.x , random.y *SpawnPos.y ,random.z * SpawnPos.z);
+            pos = pos + random;
+            var g = (GameObject)Instantiate(prefab, pos , Quaternion.identity);
+            BirdsMove b = g.GetComponent<BirdsMove>();
+            allPrefabs.Add(b);
+            b.birdsManage = this;
+        }
+        GoalPos = this.transform.position;
+    }
+    private void Update()
+    {
+        var random = Random.insideUnitSphere;
+        Vector3 pos = new Vector3(random.x * SpawnPos.x, random.y * SpawnPos.y, random.z * SpawnPos.z);
+        GoalPos = pos;
+    }
+}
---
+++ Assets/Scripts/BirdsMove.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdsMove : MonoBehaviour
+{
+    private float MoveSpeed ;
+    private List<BirdsMove> totalBoids = new List<BirdsMove>();
+    public BirdsManager birdsManage;
+    void Start()
+    {
+        MoveSpeed = Random.Range(birdsManage.MovementSpeed,birdsManage.MovementSpeed);
+    }
+    void Update()
+    {
+        transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+        Applyforce();
+    }
+    public void Applyforce()
+    {
+        totalBoids = birdsManage.allPrefabs;
+        float maxdistance = 50f;
+        Vector3 GroupCenter = Vector3.zero;
+        Vector3 Avoid = Vector3.zero;
+        int GroupSize = 0;
+        float GlobalSpeed = 0.01f;
+        foreach (var Current in totalBoids)
+        {
+            if (Current != this.gameObject)
+            {
+                float distance = Vector3.Distance(Current.transform.position, this.transform.position);
+                //if ((distance > 0) & (distance < maxdistance))
+                if (distance <= maxdistance)
+                {
+                    GroupCenter += Current.transform.position;
+                    GroupSize++;
+                    if (distance < 1.0f)
+                    {
+                        Avoid += (this.transform.position - Current.transform.position);
+                    }
+                    BirdsMove flockSpeed = Current.GetComponent<BirdsMove>();
+                    GlobalSpeed = GlobalSpeed * flockSpeed.MoveSpeed;
+                }
+            }
+        }
+        if (GroupSize > 0)
+        {
+            Vector3 Goalpos = birdsManage.GoalPos - this.transform.position;
+            GroupCenter = GroupCenter / GroupSize + Goalpos;
+            Vector3 pos = GroupCenter + Avoid;
+            Vector3 direction = pos - this.transform.position;
+            if (direction != Vector3.zero)
+            {
+                Quaternion rotPos = Quaternion.LookRotation(direction);
+                Quaternion rot = Quaternion.Slerp(this.transform.rotation, rotPos, 0.2f * Time.deltaTime);
+                transform.rotation = rot;
+            }
+        }
+    }
+}
